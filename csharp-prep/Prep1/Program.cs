4db72ce41af012f7e67934a36107bf425b3ca645
Terran-renderer <terranrenderer@gmail.com>
2025-09-17 11:59:34 -0600

    Finish hw wk 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+        string first = Console.ReadLine();
+        Console.WriteLine("What is your Last Name?");
+        string last = Console.ReadLine();
+        Console.WriteLine($"\nYour name is {last}, {first} {last}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("How bad are u failing, scale of grade?");
+        string grade = Console.ReadLine();
+        int gradeNum = int.Parse(grade);
+
+        if (gradeNum >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (gradeNum >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradeNum >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (gradeNum >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
