09dc462ca998d8064d5d3f69e8550ad35553c6c1
Terran-renderer <terranrenderer@gmail.com>
2025-09-22 09:31:47 -0600

    Made numbers game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        bool flag = true;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        Console.Write("Guess ");
+
+        while (flag == true)
+        {
+            Console.Write("");
+            int response = int.Parse(Console.ReadLine());
+
+            if (number < response)
+                Console.Write("Lower ");
+            else if (number > response)
+                Console.Write("Higher ");
+            else
+            {
+                Console.Write("CONGRATZ");
+                flag = false;
+            }
+        }
     }
 }
