b132d46ee0523e2a4d1f10f74691b99f093b65ae
Terran-renderer <terranrenderer@gmail.com>
2025-09-24 10:22:43 -0600

    Finished Prep 3,4,5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Net;
+using System.Runtime.InteropServices.Marshalling;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        bool flag = true;
+        List<int> numbers = new List<int>();
+
+        while (flag == true)
+        {
+            Console.WriteLine("Enter Number");
+            int response = int.Parse(Console.ReadLine());
+
+            if (response == 0)
+            {
+                flag = false;
+            }
+            else
+            {
+                numbers.Add(response);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine($"\nSum : {sum}");
+        Console.WriteLine($"Average: {(double)sum / numbers.Count:F2}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        {
+            hallo();
+
+            string userName = PromptUserName();
+            int userNumber = IDNumber();
+
+            int sqr = sqrrt(userNumber);
+
+            int birfYear;
+            PromptBirfYear(out birfYear);
+
+
+            DisplayResult(userName, sqr, birfYear);
+        }
+        static void hallo()
+        {
+            Console.Write("Hallo Mein Friund");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("\nYour name?");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+        static int IDNumber()
+        {
+            Console.Write("Your identification number?");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        static void PromptBirfYear(out int birfYear)
+        {
+            Console.Write("Mmh, what is your year of birth?");
+            birfYear = int.Parse(Console.ReadLine());
+        }
+        static int sqrrt(int number)
+        {
+            int sqr = number * number;
+
+            return sqr;
+        }
+        static void DisplayResult(string name, int square, int birfYear)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}.");
+            Console.WriteLine($"{name}, you will turn {2025 - birfYear} years old this year.");
+        }
+
+        
+        
     }
 }
